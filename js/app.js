ee833c62f9a3c0545f20965f770cec29a68ebea9
Jorge Barcos <jorgebarcos88@gmail.com>
2019-08-25 22:59:53 -0300

    Leyendo los datos del formulario

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 // Instanciar ambas clases
 const eventBrite = new EventBrite();
 const ui = new Interfaz();
+
+// Listener al buscador
+
+document.getElementById('buscarBtn').addEventListener('click', (e) => {
+	e.preventDefault();
+
+	// Leer el texto del input buscar
+	const textoBuscador = document.getElementById('evento').value;
+
+	// leer el select
+	const categorias = document.getElementById('listado-categorias');
+	const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+
+	// Revisar que haya algo escrito en el buscador
+	if (textoBuscador !== '') {
+		console.log('Buscado...');
+	} else {
+		console.log('no hay nada');
+	}
+});
